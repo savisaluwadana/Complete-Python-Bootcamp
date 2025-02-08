fe7a02f728cb22d59efb038f113a2e3655e97b86
savisaluwadana <82082138+savisaluwadana@users.noreply.github.com>
2025-02-08 15:48:14 +0530

    done with lists

--- 3-Data Structures/datastructures.py
+++ 3-Data Structures/datastructures.py
@@ -1,4 +1,24 @@
 #lists in python
 
 list = [1,2,3,4,5]
+print(type(list))
 
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.append("orange")
+print(fruits)
+
+print(fruits[-1])
+print(fruits[1:3])
+
+#modifying the list elements
+fruits[1] = "mango"
+print(fruits)
+
+fruits.insert(1, "grapes")
+#list methods
+#append() - adds an element to the end of the list
+fruits.append("kiwi")
+
+#insert() - adds an element at the specified position
+fruits.insert(1, "watermelon")
